0da4907d12f74be6873c6fc25fff038fe7a09030
BarrieLAJ <alhajiab.barrie@gmail.com>
2022-11-24 08:07:44 +0000

    Final UI for this baby

--- pages/admin/analytics.tsx
+++ pages/admin/analytics.tsx
@@ -3,8 +3,19 @@ import React from "react";
 import Layout from "../../src/components/Layout";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
+import { useQuery } from "@tanstack/react-query";
+import qs from "qs";
+import axios from "axios";
 
-const SmallerCard = () => {
+const SmallerCard = (props: { name: string; imgSrc: string; id: string }) => {
+	const {
+		data: productSoldCount,
+		isLoading: productSoldCountIsLoading,
+		isError: productSoldCountiIsError,
+	} = useQuery<any>([`productSoldCount${props.id}`], async () => {
+		const res = await axios.get(`http://localhost:1337/api/analytics-data/product-sold/${props.id}`);
+		return res.data;
+	});
 	return (
 		<Card
 			elevation={0}
@@ -26,10 +37,17 @@ const SmallerCard = () => {
 			<Stack spacing={1}>
 				<Avatar
 					variant="circular"
-					src=""
+					src={`http://localhost:1337${props.imgSrc}`}
 					sx={{
 						height: 84,
 						width: 84,
+					}}
+					imgProps={{
+						width: "100%",
+						style: {
+							objectFit: "cover",
+							objectPosition: "center",
+						},
 					}}
 				/>
 				<Typography
@@ -39,7 +57,7 @@ const SmallerCard = () => {
 						color: "#000",
 					}}
 				>
-					Maltina
+					{props.name}
 				</Typography>
 			</Stack>
 			<Stack spacing={0}>
@@ -59,14 +77,14 @@ const SmallerCard = () => {
 						color: "#002266",
 					}}
 				>
-					166
+					{!productSoldCountIsLoading && productSoldCount && productSoldCount.productsSoldCount}
 				</Typography>
 			</Stack>
 		</Card>
 	);
 };
 
-const BiggerCard = () => {
+const BiggerCard = (props: { title: string[]; count: string; color?: string }) => {
 	return (
 		<Card
 			elevation={0}
@@ -91,6 +109,7 @@ const BiggerCard = () => {
 					justifyContent: "center",
 					overflow: "hidden",
 					position: "relative",
+					mb: 3,
 				}}
 			>
 				<Typography
@@ -99,14 +118,82 @@ const BiggerCard = () => {
 						fontWeight: 600,
 					}}
 				>
-					588
+					{props.count}
 				</Typography>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					justifyContent: props.title.length > 1 ? "space-between" : "center",
+				}}
+			>
+				{props.title.map((t, i) => {
+					return (
+						<Typography
+							key={i}
+							sx={{
+								fontSize: "16px",
+								fontWeight: 400,
+								color: "#002266",
+							}}
+						>
+							{t}
+						</Typography>
+					);
+				})}
 			</Box>
 		</Card>
 	);
 };
 
 const Analytics = () => {
+	const {
+		data: participants,
+		isLoading: particiIsLoading,
+		isError: particiIsError,
+	} = useQuery<any>(["participantsCount"], async () => {
+		const res = await axios.get(`http://localhost:1337/api/analytics-data/participants`);
+		return res.data;
+	});
+	const {
+		data: raffleWinners,
+		isLoading: raffleWinnersIsLoading,
+		isError: raffleWinnersiIsError,
+	} = useQuery<any>(["raffleWinnersCount"], async () => {
+		const res = await axios.get(`http://localhost:1337/api/analytics-data/raffleWinners`);
+		return res.data;
+	});
+	const {
+		data: productsSoldCount,
+		isLoading: productsSoldCountIsLoading,
+		isError: productsSoldCountiIsError,
+	} = useQuery<any>(["productsSoldCount"], async () => {
+		const res = await axios.get(`http://localhost:1337/api/analytics-data/products-sold`);
+		return res.data;
+	});
+	const {
+		data: products,
+		isLoading: productsIsLoading,
+		isError: productsIsError,
+	} = useQuery<any[]>(["products"], async () => {
+		const query = qs.stringify(
+			{
+				fields: ["Name"],
+				populate: {
+					Image: {
+						fields: ["name", "url"],
+					},
+				},
+			},
+			{
+				encodeValuesOnly: true,
+			}
+		);
+		const res = await axios.get(`http://localhost:1337/api/products?${query}`);
+		console.log(res.data.data);
+		return res.data.data;
+	});
+	// console.log(participants, raffleWinners, products, productsSoldCount);
 	return (
 		<Layout title="Analytics">
 			<Container sx={{ py: 5 }} maxWidth="lg">
@@ -122,9 +209,15 @@ const Analytics = () => {
 					SLBL RAFFLE TICKET DATA ANALYTICS
 				</Typography>
 				<Stack direction="row" spacing={4} mb={10}>
-					<BiggerCard />
-					<BiggerCard />
-					<BiggerCard />
+					<BiggerCard
+						title={["Total number of participants"]}
+						count={participants ? participants.customerCount : "0"}
+					/>
+					<BiggerCard title={["Number of winners"]} count={raffleWinners ? raffleWinners.customerWinnersCount : "0"} />
+					<BiggerCard
+						title={["Number of drinks sold in raffle"]}
+						count={productsSoldCount ? productsSoldCount.productsSoldCount : "0"}
+					/>
 				</Stack>
 				<Divider
 					sx={{
@@ -134,12 +227,18 @@ const Analytics = () => {
 					}}
 				/>
 				<Stack direction="row" columnGap={4} rowGap={4} flexWrap="wrap" mb={5} mx="auto">
-					<SmallerCard />
-					<SmallerCard />
-					<SmallerCard />
-					<SmallerCard />
-					<SmallerCard />
-					<SmallerCard />
+					{!productsIsLoading &&
+						products &&
+						products.map((product) => {
+							return (
+								<SmallerCard
+									key={product.id}
+									name={product.attributes.Name}
+									imgSrc={product.attributes.Image.data.attributes.url}
+									id={product.id}
+								/>
+							);
+						})}
 				</Stack>
 			</Container>
 		</Layout>
